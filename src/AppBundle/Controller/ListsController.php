9b2d60ec42caa221c21d937aeb4f8faa9f94f86d
Mets666 <matej.sadlon@gmail.com>
2017-01-25 17:48:01 +0100

    lists controller + twig created

---
+++ src/AppBundle/Controller/ListsController.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace AppBundle\Controller;
+
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ListsController extends DefaultController
+{
+
+    /**
+     * @Route("/lists", name="lists")
+     */
+    public function listsAction(Request $request)
+    {
+
+    }
+}
