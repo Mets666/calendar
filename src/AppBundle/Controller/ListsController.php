d212a5bba92f485f13f87c32947d04fd61b098f1
Mets666 <matej.sadlon@gmail.com>
2017-02-06 17:56:08 +0100

    lists edit

--- src/AppBundle/Controller/ListsController.php
+++ src/AppBundle/Controller/ListsController.php
@@ -12,9 +12,12 @@ class ListsController extends DefaultController
 {
 
     /**
-     * @Route("/lists", name="lists")
+     * @Route("/lists/{listId}", name="lists", defaults={"listId" = 0})
+     * @param Request $request
+     * @param $id
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function listsAction(Request $request)
+    public function listsAction(Request $request, $listId)
     {
 
         $todoListRepository = $this->get('app.todo_list.repository');
@@ -22,7 +25,24 @@ class ListsController extends DefaultController
         /** @var \AppBundle\Entity\User $user */
         $user = $this->get('security.token_storage')->getToken()->getUser();
 
+
+        /** @var \AppBundle\Entity\TodoList[] $todoLists */
         $todoLists = $user->getTodoLists();
+
+        $selectedList = null;
+
+        if(!empty($todoLists)){
+            if($listId == 0) {
+                $selectedList = $todoLists[0];
+            }
+            else{
+                foreach ($todoLists as $todoList){
+                    if ($todoList->getId() == $listId){
+                        $selectedList = $todoList;
+                    }
+                }
+            }
+        }
 
         $todoListForm = $this->createForm(TodoListType::class, array(), array(
             'action' => $this->generateUrl('add_category')
@@ -31,7 +51,7 @@ class ListsController extends DefaultController
         return $this->render('default/lists.html.twig', array(
             'todo_list_form' => $todoListForm->createView(),
             'todo_lists' => $todoLists,
-
+            'selected_list' => $selectedList
         ));
     }
     
